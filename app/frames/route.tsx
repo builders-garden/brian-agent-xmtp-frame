587a32e234e72c00bc6e522969dee5d76ad17268
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-02 20:08:40 +0200

    Updated main frame image and endpoint

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -3,17 +3,13 @@ import { frames } from "./frames";
 
 const handleRequest = frames(() => {
   return {
-    image: (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        Brian Agent XMTP Frame
-      </div>
-    ),
-    buttons: [],
+    title: "Main Frame",
+    image: `${process.env.NEXT_PUBLIC_BASE_URL}/images/main.png`,
+    buttons: [
+      <Button key="0" action="link" target="https://www.brianknows.org/">
+        Learn more about Brian
+      </Button>,
+    ],
   };
 });
 
