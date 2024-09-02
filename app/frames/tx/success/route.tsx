4778465fdd11bac5559575be53e8e66c301636a2
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-02 16:06:20 +0200

    Added image and button to learn more about Brian

--- app/frames/tx/success/route.tsx
+++ app/frames/tx/success/route.tsx
@@ -2,15 +2,14 @@ import { Button } from "frames.js/next";
 import { frames } from "../../frames";
 
 const handleRequest = frames(async (ctx) => {
-  const inputText = ctx.message?.inputText;
-  const button = ctx.message?.buttonIndex;
   return {
-    image: (
-      <div tw="flex flex-col">
-        <div tw="flex flex-col">All requests completed!</div>
-      </div>
-    ),
-    buttons: [],
+    title: "All completed",
+    image: `${process.env.NEXT_PUBLIC_BASE_URL}/images/all-completed.png`,
+    buttons: [
+      <Button key="0" action="link" target="https://www.brianknows.org/">
+        Learn more about Brian
+      </Button>,
+    ],
   };
 });
 
