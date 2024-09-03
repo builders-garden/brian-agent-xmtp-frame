6b146db9c97f94bf4828ffdbc1d1dbb52c6f9094
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-04 00:04:13 +0200

    Improved types

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,21 @@
+export interface Step {
+  from: `0x${string}`;
+  to: `0x${string}`;
+  data: `0x${string}`;
+  value: string;
+}
+
+export interface Request {
+  action: string;
+  description: string;
+  chainId: number;
+  tokenIn: `0x${string}`;
+  steps: Step[];
+  stepsLength: number;
+}
+
 export interface TransactionsDataType {
   address: string;
-  requests: {
-    description: string;
-    chainId: number;
-    tokenIn: `0x${string}`;
-    steps: {
-      from: `0x${string}`;
-      to: `0x${string}`;
-      data: `0x${string}`;
-      value: string;
-    }[];
-    stepsLength: number;
-  }[];
+  requests: Request[];
   requestsLength: number;
 }
