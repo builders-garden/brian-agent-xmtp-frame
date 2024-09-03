566d7362ca6be703195eb3b4a1d4269204e3d9eb
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-03 20:51:00 +0200

    Minor changes

--- app/frames/tx/route.tsx
+++ app/frames/tx/route.tsx
@@ -2,18 +2,17 @@ import { Abi } from "viem";
 import { frames } from "../frames";
 import { erc20Abi } from "./contracts/erc20-abi";
 import { transaction } from "frames.js/core";
-import { baseSepolia, base } from "viem/chains";
 import { getRedisClient } from "@/lib/redis";
 import { TransactionsDataType } from "@/lib/types";
 
 export const POST = frames(async (ctx) => {
-  const txId = ctx.url.searchParams.get("id");
+  const redisId = ctx.url.searchParams.get("id");
   const step = parseInt(ctx.url.searchParams.get("step") ?? "0");
   const request = parseInt(ctx.url.searchParams.get("request") ?? "0");
 
   // If the tx id is not present, throw an error
-  if (!txId) {
-    throw new Error("Invalid txId");
+  if (!redisId) {
+    throw new Error("Invalid redis ID");
   }
 
   // Getting the redis client
@@ -21,13 +20,13 @@ export const POST = frames(async (ctx) => {
 
   // Get the transaction data from the redis database
   // If no data is found, throw an error
-  const txData: TransactionsDataType | null = await redisClient.get(txId);
+  const txData: TransactionsDataType | null = await redisClient.get(redisId);
   console.log("txData: ", txData);
   if (!txData) {
     throw new Error("No transaction data found");
   }
 
-  // Get the current request and step from the transaction data
+  // Get the current step and request from the transaction data
   const currentRequest = txData.requests[request];
   const currentStep = currentRequest.steps[step];
 
