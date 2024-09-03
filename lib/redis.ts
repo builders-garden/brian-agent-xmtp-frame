4bdcaf71d6b9262f406d424bd94a5f1a654fd3ef
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-03 20:51:07 +0200

    Added comment

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,9 @@
 import { Redis } from "@upstash/redis";
 
+/**
+ * Gets the Redis client
+ * @returns The Redis client
+ **/
 export const getRedisClient = (): Redis => {
   const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL,
