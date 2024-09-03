e6304e2fc8e1735f3979425beb2e7d1921668106
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-09-04 00:09:35 +0200

    Added 1:15 minutes of waiting while registering an ENS domain

--- app/frames/brian-tx/route.tsx
+++ app/frames/brian-tx/route.tsx
@@ -37,11 +37,18 @@ const handleRequest = frames(async (ctx) => {
         ? txData.requests[request - 1].chainId
         : txData.requests[request].chainId;
 
+    // Get the action of the request where the transaction was actually made
+    const action =
+      step === 0 && request > 0
+        ? txData.requests[request - 1].action
+        : txData.requests[request].action;
+
     try {
       const publicClient = getPublicClient(chainId.toString());
       const transaction = await publicClient.getTransactionReceipt({
         hash: txId,
       });
+      // If the transaction is reverted, show the transaction reverted frame with the option to try again
       if (transaction.status === "reverted") {
         return {
           title: "Transaction Reverted",
@@ -76,7 +83,9 @@ const handleRequest = frames(async (ctx) => {
             </Button>,
           ],
         };
-      } else if (transaction.status === "success" && end === "true") {
+      }
+      // If the transaction is successful and the end is true, show the all completed frame
+      else if (transaction.status === "success" && end === "true") {
         return {
           title: "All completed",
           image: `${process.env.NEXT_PUBLIC_BASE_URL}/images/all-completed.png`,
@@ -87,8 +96,31 @@ const handleRequest = frames(async (ctx) => {
           ],
         };
       }
+      // If the transaction is successful, the action is ENS Registration and the step is 0
+      // Get the timestamp from the block and check if the transaction was made more than 1 minute and 15 seconds ago
+      // Otherwise, show the transaction waiting frame again by throwing an error
+      else if (
+        transaction.status === "success" &&
+        action === "ENS Registration" &&
+        step - 1 === 0 // This is the first step of the ENS Registration
+      ) {
+        const block = await publicClient.getBlock({
+          blockNumber: transaction.blockNumber,
+        });
+
+        // Convert Date.now() to bigint and perform the subtraction
+        // block.timestamp is in seconds, so multiply it by 1000 to convert it to milliseconds
+        const timeDifference =
+          BigInt(Date.now()) - block.timestamp * BigInt(1000);
+
+        if (timeDifference < BigInt(75000)) {
+          throw new Error(
+            "The transaction must wait for 1 minute and 15 seconds because it's the ENS Registration's first step"
+          );
+        }
+      }
     } catch (error) {
-      // If the transaction is not found yet, show the transaction waiting screen
+      // If the transaction is not found yet, show the transaction waiting frame
       return {
         title: "Transaction Waiting",
         image: (
@@ -156,6 +188,7 @@ const handleRequest = frames(async (ctx) => {
       <Button
         action="tx"
         target={`/tx?id=${redisId}&step=${step}&request=${request}`}
+        // The end is true if the request is the last one and the step is the last one
         post_url={
           request < requestsLength - 1 && step === stepsLength - 1
             ? `/brian-tx?id=${redisId}&step=0&request=${request + 1}&end=false`
